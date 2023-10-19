b9c5ec465c5c8bb869b7a0b5cf4a4b698cd6969c
sova <mahatoshobha143@gmail.com>
2023-10-19 21:38:47 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(debugShowCheckedModeBanner: false,
-    home: Level1Screen());
+    home: SplashScreen());
   }
 }
